6591f330acd35dcfd9af7c80816bb45b9b415a50
Mmd4J <hosun1451@gmail.com>
2025-10-04 13:40:30 +0330

    Initial commit

---
+++ src/main/java/ir/mmd4j/kirbyframework/utils/StringUtil.java
@@ -0,0 +1,41 @@
+package ir.mmd4j.kirbyframework.utils;
+
+import lombok.experimental.UtilityClass;
+import org.bukkit.ChatColor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class StringUtil {
+    /**
+     * a shorter version of ChatColor.translateAlternateColorCodes('&',text)
+     * @param text the text trying to translate its color
+     * @return
+     */
+    public String t(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+    /**
+     * returns list of possible words that fit the word also cuts the word itself.
+     * @param word given non completed word
+     * @param words list of possible words to complete
+     * @return
+     */
+    public List<String> completedWords(String word, List<String> words) {
+        return words.stream().filter(w -> w.toLowerCase().startsWith(word.toLowerCase())).map(String::toLowerCase)
+                .map(w -> w.substring(word.length())).collect(Collectors.toList());
+    }
+
+    /**
+     * Shows the possible list of words that are started by the specified word
+     * @param word given word to check
+     * @param words possible wordlist
+     * @return
+     */
+    public List<String> listWordsStartWith(String word, List<String> words) {
+        return words.stream().filter(w -> w.toLowerCase().startsWith(word.toLowerCase())).map(String::toLowerCase)
+                .collect(Collectors.toList());
+    }
+}
